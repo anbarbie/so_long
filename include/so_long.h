9a1794d9052fccd45e172d1665b72aca3c5f3a34
anbarbie <silio@DESKTOP-JLEVGE9.localdomain>
2021-10-10 22:13:25 +0200

    start

---
+++ include/so_long.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antbarbi <antbarbi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 21:50:45 by antbarbi          #+#    #+#             */
+/*   Updated: 2021/10/10 21:50:45 by antbarbi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "../minilibx_linux/mlx.h"
+# include "../minilibx_linux/mlx_int.h"
+# include <math.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdint.h>
+
+#endif
